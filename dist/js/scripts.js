f7e2b44e27febcc9999b99557bd9167a99adf338
Mico Infante <micoinfante1997@gmail.com>
2020-07-22 22:38:31 +0800

    Updating info

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-    * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+    * Start Bootstrap - Resume v6.0.1 (undefined)
+    * Copyright 2013-2020 Mico Infante
+    * Licensed under undefined (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
     */
     (function ($) {
     "use strict"; // Start of use strict
